e69a91402de42b2dac08f5b10673b2504b3978ef
sai-prasad-1919 <psai6269@gmail.com>
2026-05-29 02:53:37 +0530

    Added the authentication for the web application

---
+++ src/components/ProtectedRoute.jsx
@@ -0,0 +1,21 @@
+import { Navigate, Outlet, useLocation } from "react-router-dom";
+
+import { isAuthenticated } from "../utils/auth";
+
+function ProtectedRoute() {
+  const location = useLocation();
+
+  if (!isAuthenticated()) {
+    return (
+      <Navigate
+        to="/login"
+        replace
+        state={{ from: location }}
+      />
+    );
+  }
+
+  return <Outlet />;
+}
+
+export default ProtectedRoute;
